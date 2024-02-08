20ff0903da3698a0b5648cdc54c0d365fd14d754
Jose Montes De Oca Morfin <112668683+JoseMDO@users.noreply.github.com>
2024-02-08 17:18:51 +0000

    finished

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,6 +2,7 @@ class DepartmentsController < ApplicationController
   def index
     @departments = Department.all.order({ :created_at => :desc })
 
+
     render({ :template => "departments/index" })
   end
 
